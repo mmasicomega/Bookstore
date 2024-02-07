50c742daac4d9752730479fefcf68a98d355b59a
Martin Mašić <MMasic@omega-software.hr>
2024-02-07 09:21:03 +0100

    Added dashboard, RestGenerator, seversal new entities.

--- src/Bookstore.Service/Program.cs
+++ src/Bookstore.Service/Program.cs
@@ -7,14 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(o => o.CustomSchemaIds(type => type.ToString()));
 
 builder.Services.AddRhetosHost((serviceProvider, rhetosHostBuilder) => rhetosHostBuilder
         .ConfigureRhetosAppDefaults()
         .UseBuilderLogProviderFromHost(serviceProvider)
         .ConfigureConfiguration(cfg => cfg.MapNetCoreConfiguration(builder.Configuration)))
     .AddAspNetCoreIdentityUser()
-    .AddHostLogging();
+    .AddHostLogging()
+    .AddDashboard()
+    .AddRestApi(o =>
+    {
+        o.BaseRoute = "rest";
+        o.GroupNameMapper = (conceptInfo, controller, oldName) => "v1";
+    });
 
 var app = builder.Build();
 
@@ -29,7 +35,14 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseRhetosRestApi();
+
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapRhetosDashboard();
+}
 
 app.Run();
 
